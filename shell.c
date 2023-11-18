db77396bf91d567aad1900619133be67c5d93b0a
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-11-17 19:31:51 -0800

    code to pass the betty check

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - function that passes the betty check
+ * @ac: argument counter
+ * @argv: argument list
+ *
+ * Return: always return 0
+ */
+int main(int ac, char **argv)
+{
+	return (0);
+}
+
